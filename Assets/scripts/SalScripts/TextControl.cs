4daeb9562b72756ff4d76a569a13914cfa1c186f
Sally Chen <jc11618@nyu.edu>
2023-11-04 16:24:21 -0400

    New sprites created in Sprite folder named "SalSprites", sprites added to scene named "SalScene"

--- Assets/scripts/SalScripts/TextControl.cs
+++ Assets/scripts/SalScripts/TextControl.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TextControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private TMP_Text dialogueText; //Get TMP_Text dialogue as dialogueText here
+    private string textContent; //store content that is going to be written
+    private float typeTime; //store time of typing each character
+    private int charIndex; //track the index of character in a string
+    private float timer;
+   
+
+    private void TypingText(TMP_Text dialogueText, string textContent, float typeTime)
+    {
+        this.dialogueText = dialogueText;
+        this.textContent = textContent;
+        this.typeTime = typeTime;
+        charIndex = 0;
+    }
+
     void Start()
     {
         
@@ -13,6 +29,16 @@ public class TextControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (dialogueText != null) //if there is a dialogue text object
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f) //time to display next character (typing)
+            {
+                timer += typeTime; //reset timer
+                charIndex++;
+                dialogueText.text = textContent.Substring(0, charIndex); //extract character of the text
+            }
+        }
     }
 }
